21c00ad5222c2fe776710b3e6485f3bb0a3c960c
fadhilghazyelhaq-cloud <fadhilghazyelhaq@smkitihsanulfikri.sch.id>
2026-01-28 08:28:47 +0700

    Create biodata_fadhilghazy.py

---
+++ biodata_fadhilghazy.py
@@ -0,0 +1,14 @@
+print("=======================================")
+print("Program Biodata Sederhana Dengan Python")
+print("=======================================")
+
+nama = "Fadhil Ghazy El Haq"
+hobi = "Olahraga"
+alamat = "Jawa Tengah"
+kota = "Magelang"
+
+print("Nama Lengkap  :", nama)
+print("Alamat        :", alamat, "\n                Kota ", kota)
+print("Hobi          :", hobi)
+
+print("=======================================")
